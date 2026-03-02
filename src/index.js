35a60680accaa11bdc14a6475c4671b7390da865
mungeh <mungehlewisnjeh@gmail.com>
2026-03-02 22:10:51 +0100

    corrected error1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // Math utility module
 
-                                                     function add(a, b) {
-        return a + b;
-        }
+function add(a, b) {
+  return a + b;
+}
 
-                                                       function subtract(a, b) {
+function subtract(a, b) {
   return a - b;
 }
 
 function multiply(a, b) {
-                             return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-  i                                                                                                                                                                  f (b === 0) {
+  if (b === 0) {
     throw new Error('Division by zero is not allowed');
   }
   return a / b;
