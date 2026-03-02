bbbfead0083260565995d887510cd13c8bbb7426
mungeh <mungehlewisnjeh@gmail.com>
2026-03-02 22:03:58 +0100

    error2

--- src/index.js
+++ src/index.js
@@ -8,16 +8,17 @@ function subtract(a, b) {
   return a - b;
 }
 
-function multiply(a, b) {
+function multiply(a, b) 
+{
   return a * b;
 }
 
 function divide(a, b) {
-  if (b === 0) {
+  if (b === 0) 
+    {
     throw new Error('Division by zero is not allowed');
   }
   return a / b;
 }
 
-module.exports = { add, subtract, multiply, divide };
 module.exports = { add, subtract, multiply, divide };
