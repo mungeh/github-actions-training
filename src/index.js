ad8f37116635e43f1eac29112caf17067897ddb5
mungeh <mungehlewisnjeh@gmail.com>
2026-03-02 22:09:06 +0100

    error3

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 // Math utility module
 
-function add(a, b) {
-  return a + b;
-}
+                                                     function add(a, b) {
+        return a + b;
+        }
 
-function subtract(a, b) {
+                                                       function subtract(a, b) {
   return a - b;
 }
 
-function multiply(a, b) 
-{
-  return a * b;
+function multiply(a, b) {
+                             return a * b;
 }
 
 function divide(a, b) {
-  if (b === 0) 
-    {
+  i                                                                                                                                                                  f (b === 0) {
     throw new Error('Division by zero is not allowed');
   }
   return a / b;
 }
 
-module.exports = { add, subtract, multiply, divide };
+module.exports = { add, subtract, multiply, divide };  
